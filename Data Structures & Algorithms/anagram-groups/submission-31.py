e61929687471dedf72e16f768219f2bda75aa3f7
ik4ur11-ui <ik4ur11@gmail.com>
2026-09-18 21:59:19 +0100

    Add: anagram-groups - submission-31

---
+++ Data Structures & Algorithms/anagram-groups/submission-31.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for s in strs:
+            universal = ''.join(sorted(s))
+            anagrams[universal] = anagrams.get(universal,[])
+            anagrams[universal].append(s)
+            
+
+        categories = list(anagrams.values())
+
+        output = sorted(categories, key=len)
+        return output
