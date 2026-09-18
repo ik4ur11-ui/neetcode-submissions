9e0cef25480a018a4c7f233260d7ce8421fa3105
ik4ur11-ui <ik4ur11@gmail.com>
2026-09-18 22:19:59 +0100

    Add: anagram-groups - submission-35

---
+++ Data Structures & Algorithms/anagram-groups/submission-35.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            universal = ''.join(sorted(s))
+            #anagrams[universal] = anagrams.get(universal,[])
+            anagrams[universal].append(s)
+            
+        categories = list(anagrams.values())
+
+        return sorted(categories, key=len)
